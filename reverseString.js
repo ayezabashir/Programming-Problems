20317a9ec972021789c518d9ef7e3457d5d8749d
Ayeza <ayezabashir442@gmail.com>
2023-04-10 23:26:09 +0500

    reverse string

---
+++ reverseString.js
@@ -0,0 +1,7 @@
+var reverseString = function (s) {
+    s.reverse();
+    return s;
+};
+
+console.log(reverseString(["h", "e", "l", "l", "o"]));
+console.log(reverseString(["H", "a", "n", "n", "a", "h"]));
