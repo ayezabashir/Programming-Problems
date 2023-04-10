ecb2c4b313c2e0b15cb63ce4f71750fcdc93582d
Ayeza <ayezabashir442@gmail.com>
2023-04-10 23:41:24 +0500

    Fizz Buzz

---
+++ FizzBuzz.js
@@ -0,0 +1,18 @@
+var fizzBuzz = function (n) {
+    var arr = [];
+    for (let i = 1; i <= n; i++) {
+        if ((i % 3 === 0) && (i % 5 === 0)) {
+            arr.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            arr.push("Fizz");
+        } else if (i % 5 === 0) {
+            arr.push("Buzz");
+        } else {
+            arr.push("" + i + "");
+        }
+    }
+    return arr;
+};
+
+console.log(fizzBuzz(3));
+console.log(fizzBuzz(15));
