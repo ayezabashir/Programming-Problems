cadb647680f2fe71c8b73e9375ec93e67f73c318
Ayeza <ayezabashir442@gmail.com>
2023-05-25 20:58:49 +0500

    another method

---
+++ firstUniqueCharacter2.js
@@ -0,0 +1,24 @@
+var firstUniqChar = function (s) {
+    let charCounts = {};
+
+    // Count the frequency of each character
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        charCounts[char] = charCounts[char] ? charCounts[char] + 1 : 1;
+    }
+
+    // Find the first unique character and return its index
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (charCounts[char] === 1) {
+            return i;
+        }
+    }
+
+    return -1; // No unique character found
+};
+
+
+console.log(firstUniqChar("leetcode"));
+console.log(firstUniqChar("loveleetcode"));
+console.log(firstUniqChar("aabb"));
