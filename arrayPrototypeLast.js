f7420ffde92f0cb6ac063ecda6a69d90079172dc
Ayeza <ayezabashir442@gmail.com>
2023-08-05 23:49:00 +0500

    using ternary operator

--- arrayPrototypeLast.js
+++ arrayPrototypeLast.js
@@ -1,8 +1,15 @@
-Array.prototype.last = function () {
+// Using if statement
+/* Array.prototype.last = function () {
     if (this.length === 0) {
         return -1;
     }
     return this[this.length - 1];
+};*/
+
+// Using ternary Operator
+
+Array.prototype.last = function () {
+    return this.length === 0 ? -1 : this[this.length - 1];
 };
 
 const array1 = JSON.parse("[1, 2, 3]");
