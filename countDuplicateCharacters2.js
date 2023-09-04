7704d8a61d73b07de7e2e9430b0dda7c1c5de227
Ayeza <ayezabashir442@gmail.com>
2023-09-05 00:27:59 +0500

    count duplicates

---
+++ countDuplicateCharacters2.js
@@ -0,0 +1,10 @@
+// using regular expressions
+function duplicateCount(text) {
+    return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+}
+
+console.log(duplicateCount('abcde'));
+console.log(duplicateCount('aabbcde'));
+console.log(duplicateCount('aabBcde'));
+console.log(duplicateCount('indivisibility'));
+console.log(duplicateCount('aA11'));
