c458bea8de45da6dc9ebeda795fc29ea9c7bcace
Ayeza Bashir <ayezabashir442@gmail.com>
2024-07-13 22:00:18 +0500

    sleep.js

--- sleep.js
+++ sleep.js
@@ -1,11 +1,10 @@
-function sleepAsync(millis) {
-    return new Promise(resolve => setTimeout(resolve, millis));
+async function sleep(millis) {
+    return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(millis);
+    }, millis);
+  });
 }
 
-async function main() {
-    console.log("Sleeping for 1000 milliseconds...");
-    await sleepAsync(1000);
-    console.log("Awake now!");
-}
-
-main();
+let t = Date.now()
+sleep(100).then(() => console.log(Date.now() - t)) // 100
