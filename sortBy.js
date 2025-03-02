d06879c753d83d6c7af933e5dad8c16a3c4963a9
Ayeza Bashir <ayezabashir442@gmail.com>
2025-03-02 17:02:35 +0500

    Update sortBy.js

--- sortBy.js
+++ sortBy.js
@@ -10,12 +10,6 @@ var sortBy = function(arr, fn) {
     return arr;
 };
 
-// Example Test Cases
 console.log(sortBy([5, 4, 1, 2, 3], (x) => x)); 
-// Output: [1, 2, 3, 4, 5]
-
 console.log(sortBy([{x: 1}, {x: 0}, {x: -1}], (d) => d.x)); 
-// Output: [{x: -1}, {x: 0}, {x: 1}]
-
 console.log(sortBy([[3, 4], [5, 2], [10, 1]], (x) => x[1])); 
-// Output: [[10, 1], [5, 2], [3, 4]]
