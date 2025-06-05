fa4935ef6afbae6bddc20eaf3028088e5d60281d
Ayeza Bashir <ayezabashir442@gmail.com>
2025-06-05 20:01:40 +0500

    Update mergeArrays.js

--- mergeArrays.js
+++ mergeArrays.js
@@ -1,22 +1,16 @@
 var merge = function(nums1, m, nums2, n) {
-    let temp;
-    let nums3 = [];
-    for (let i = 0; i < m; i++) {
-        nums3.push(nums1[i]);
-    }
-    for (let i = 0; i < n; i++) {
-        nums3.push(nums2[i]);
-    }
-    for (let i = 0; i < nums3.length - 1; i++) {
-        for (let j = 0; j < nums3.length - 1 - i; j++) {
-            if (nums3[j] > nums3[j + 1]) {
-                temp = nums3[j];
-                nums3[j] = nums3[j + 1];
-                nums3[j + 1] = temp;
-            }
+    let i = m - 1;        
+    let j = n - 1;         
+    let k = m + n - 1;    
+    while (j >= 0) {
+        if (i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
         }
+        k--;
     }
-    return nums3;
 };
-
 console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3)); 
